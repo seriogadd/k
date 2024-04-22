a28121cab0d2e68a26e72cf08c403157a01b6f9d
serega <sergejtolokolnikov19@gmail.com>
2024-04-22 13:08:13 +0300

    new file

---
+++ 3.py
@@ -0,0 +1,41 @@
+class FlatIterator:
+
+    def __init__(self, nested_list):
+        self.stack = nested_list[::-1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.stack:
+            current = self.stack.pop()
+            if isinstance(current, list):
+                self.stack.extend(current[::-1])
+            else:
+                return current
+        raise StopIteration
+
+list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
